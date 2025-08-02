4f4eb94a14449882507f8faf2cd3fdfc37c7afcb
atharvdsingh <dsingh9450492600@gmail.com>
2025-08-02 21:31:59 +0530

    project yesteryear

---
+++ server/src/models/user.models.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from 'mongoose'
+
+userSchema=new Schema({
+    userName:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        index:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+    },password:{
+        type:String,
+        required:[true,'password is not given']
+    },refreshToken:{
+        type:String
+    }
+},{timestamps:true})
+
+
+export const User=mongoose.model('User',userSchema)
