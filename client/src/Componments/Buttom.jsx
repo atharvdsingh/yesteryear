85d4fd32cd032b1409bba987e42e2f07a110f68f
atharvdsingh <dsingh9450492600@gmail.com>
2025-11-30 00:27:50 +0530

    change in font

--- client/src/Componments/Buttom.jsx
+++ client/src/Componments/Buttom.jsx
@@ -2,7 +2,7 @@
 
 function Buttom({ text,container }) {
   return (
-    <div className="flex cursor-pointer items-center justify-center gap-2 px-6 py-2.5 text-sm font-medium text-black bg-white rounded-full hover:bg-gray-200 active:scale-95 transition-all duration-200 shadow-[0_0_20px_rgba(255,255,255,0.1)] hover:shadow-[0_0_25px_rgba(255,255,255,0.2)]">
+    <div className="flex cursor-pointer items-center justify-center gap-2 hover:gap-4 px-6 py-2.5 text-sm font-medium text-black bg-white rounded-full hover:bg-gray-200 active:scale-95 transition-all duration-200 shadow-[0_0_20px_rgba(255,255,255,0.1)] hover:shadow-[0_0_25px_rgba(255,255,255,0.2)]">
         {container}
       {text}
     </div>
